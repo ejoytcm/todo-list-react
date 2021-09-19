83b09cf3dfeefbd0d6b1ed34cdf890bd7a309227
TCM Instructor <ejoy@thecodemastery.com>
2021-09-19 12:21:03 +0530

    Todolists + change handler

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { todos } from "./componets/sampleData/todoList";
+import TodoList from "./componets/TodoLists/TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App  extends React.Component {
+
+  state = {
+    todos: [...todos]
+  }
+
+  updateTodoHandler = (todoId) => {
+    const updatedTodos = [...this.state.todos];
+    const todoListIndex = updatedTodos.findIndex(todo => todo.id === todoId);
+    const updatedTodo = {...updatedTodos[todoListIndex]};
+    let updatedTodoStatus = updatedTodo.status;
+    let updatedTodoCssClass = updatedTodo.cssClass;
+    if(updatedTodoStatus === "overdue" || updatedTodoStatus === "upcoming") {
+      updatedTodoStatus = "inprogress";
+      updatedTodoCssClass = "alert-primary";
+    } else if(updatedTodoStatus === "inprogress") {
+      updatedTodoStatus = "completed";
+      updatedTodoCssClass = "alert-success";
+    }
+    updatedTodo.status = updatedTodoStatus;
+    updatedTodo.cssClass = updatedTodoCssClass;
+    updatedTodos[todoListIndex] = updatedTodo;
+    this.setState({ todos: updatedTodos });
+  }
+
+  render() {
+    return (
+      <div className="container mt-5">
+        <h5 className="text-center mb-5">Todo Lists</h5>
+        <TodoList 
+          todos={this.state.todos}
+          updateTodo={(todoId) => this.updateTodoHandler(todoId)}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/componets/sampleData/todoList.js
@@ -0,0 +1,26 @@
+export const todos = [
+    {
+        id: "todo-01",
+        status: "completed",
+        cssClass: "alert-success",
+        description: "Wakeup at 6AM and do morning strech."
+    },
+    {
+        id: "todo-02",
+        status: "overdue",
+        cssClass: "alert-warning",
+        description: "Get ready for work and take pick Jane on the way."
+    },
+    {
+        id: "todo-03",
+        status: "inprogress",
+        cssClass: "alert-primary",
+        description: "Empty inbox and attend daily standup meeting."
+    },
+    {
+        id: "todo-04",
+        status: "upcoming",
+        cssClass: "alert-secondary",
+        description: "Interview candidate #100501."
+    },
+]
